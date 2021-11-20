72c509ecf0774750a1c2686cc097852918738dba
Brian Duong <brianminhduong@gmail.com>
2021-11-20 16:35:43 +0000

    Create test file for index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+/*
+Steps
+Fetch our IP Address
+Fetch the geo coordinates (Latitude & Longitude) for our IP
+Fetch the next ISS flyovers for our geo coordinates
+
+Example result:
+Next pass at Fri Jun 01 2021 13:01:35 GMT-0700 (Pacific Daylight Time) for 465 seconds!
+Next pass at Fri Jun 01 2021 14:36:08 GMT-0700 (Pacific Daylight Time) for 632 seconds!
+Next pass at Fri Jun 01 2021 16:12:35 GMT-0700 (Pacific Daylight Time) for 648 seconds!
+Next pass at Fri Jun 01 2021 17:49:29 GMT-0700 (Pacific Daylight Time) for 648 seconds!
+Next pass at Fri Jun 01 2021 19:26:12 GMT-0700 (Pacific Daylight Time) for 643 seconds!
+*/
+
+const ipAddressUrl = 'https://api.ipify.org?format=json';
+
+const fetchMyIP = function(callback) {
+  request('ipAddressUrl', (error, response, body) => {
+    if (error) {
+      return callback(error, null)
+    };
+
+    return callback(error,body["ip"]); 
+  })
+}
+
+module.exports = {
+  fetchMyIP
+}

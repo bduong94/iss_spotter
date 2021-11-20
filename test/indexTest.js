439576c8096b19babd2c8a7f8342107b17190c91
Brian Duong <brianminhduong@gmail.com>
2021-11-20 16:49:23 +0000

    Add error checks to fetchIP function

---
+++ test/indexTest.js
@@ -0,0 +1,11 @@
+const { fetchMyIP } = require('../index');
+const { assert } = require('chai');
+
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log('It worked! Returned IP:', ip);
+});
